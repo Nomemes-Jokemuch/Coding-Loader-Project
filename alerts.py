af161d85a37e1195c0256833751cdf5fa06d1b3b
56hg7t83 <153200435+Nomemes-Jokemuch@users.noreply.github.com>
2024-03-31 15:13:50 +1000

    !

--- alerts.py
+++ alerts.py
@@ -6,20 +6,21 @@ path = "Alarm"
 numero = ["04"]
 
 def report_sorting(report_path: str, date_sorting: str):
-    with open(report_path, "r") as report:
-        file_name = Path(report_path).stem
-        yyyymmdd = file_name[0:8]
-        hhmmss = file_name[9:15]
-        if yyyymmdd == date_sorting and (80000 <= int(hhmmss) <= 235959):
-            return 1
-        elif (yyyymmdd[0:6] + (int(yyyymmdd[7:8]) + 1)) == date_sorting and (0 <= int(hhmmss) <= 7959):
-            return 1
+    file_name = Path(report_path).stem
+    yyyymmdd = file_name[0:8]
+    hhmmss = file_name[9:15]
+    if yyyymmdd == date_sorting and (80000 <= int(hhmmss) <= 235959):
+        return 1
+    elif (yyyymmdd[0:6] + (int(yyyymmdd[7:8]) + 1)) == date_sorting and (0 <= int(hhmmss) <= 7959):
+        return 1
 
-def alert_list(folder_path, search_string_list):
+def alert_list(folder_path, search_string_list, date_sorting):
     alerts_list = []
     
     for file_name in os.listdir(folder_path):
         with open(os.path.join(folder_path, file_name), "r") as file:
+            if report_sorting(os.path.abspath(file.name), date_sorting) != 1:
+                continue
             file_content = file.read()
             for string in file_content.splitlines():
                 string = get_rid_of_space(string)
--- funcs.py
+++
@@ -1,2 +0,0 @@
-def get_rid_of_space(string):
-    return string[: string.find("          ")]
