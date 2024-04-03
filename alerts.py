75c195a35e0452e9e222ee49fdf277cb8f0059ed
56hg7t83 <153200435+Nomemes-Jokemuch@users.noreply.github.com>
2024-04-03 18:44:06 +1000

    Теперь нормальная проверка времени.

--- alerts.py
+++ alerts.py
@@ -4,24 +4,30 @@ from funcs import get_rid_of_space
 
 path = "Alarm"
 numeros = ["04-RIGHTLADDERDOWN  3"]
-date = '20240316'
+date = "20240315"
+
 def report_sorting(report_path: str, date_sorting: str):
     file_name = Path(report_path).stem
     yyyymmdd = file_name[0:8]
     hhmmss = file_name[9:15]
     if yyyymmdd == date_sorting and (80000 <= int(hhmmss) <= 235959):
         return 1
-    elif (yyyymmdd[0:6] + str(int(yyyymmdd[7:8]) + 1)) == date_sorting and (0 <= int(hhmmss) <= 7959):
+    elif (yyyymmdd[0:6] + str(int(yyyymmdd[6:8]) - 1)) == date_sorting and (
+        0 <= int(hhmmss) <= 75959
+    ):
         return 1
     else:
         return 0
 
 def alert_list(folder_path, search_string_list, date_sorting, sort_date=True):
     alerts_list = []
-    
+
     for file_name in os.listdir(folder_path):
         with open(os.path.join(folder_path, file_name), "r") as file:
-            if report_sorting(os.path.abspath(file.name), date_sorting) == 0 and sort_date:
+            if (
+                report_sorting(os.path.abspath(file.name), date_sorting) == 0
+                and sort_date
+            ):
                 continue
             file_content = file.read()
             for string in file_content.splitlines():
@@ -35,10 +41,10 @@ def alert_list(folder_path, search_string_list, date_sorting, sort_date=True):
                                     alert[2].append(file_name)
                         else:
                             alerts_list.append([string, 1, [file_name]])
-                        
+
     return alerts_list
 
-print("begin")
+print("—")
 
 for i in alert_list(path, numeros, date):
     for j in i[2]:
